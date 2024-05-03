cd0c6802f334be47c3d66150b3caa2410090c160
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-05-03 14:09:51 +0300

    modified: routers/ devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :doctors
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_for :doctors, controllers: {
+    sessions: 'doctors/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
