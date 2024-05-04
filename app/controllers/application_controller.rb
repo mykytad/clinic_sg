f27d74e2258a772a339220380fd4054823702bc4
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-05-04 13:04:05 +0300

    log in phone-password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :name])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:phone])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :doctors, controllers: {
     sessions: 'doctors/sessions'
   }
+  root "home#index"
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
