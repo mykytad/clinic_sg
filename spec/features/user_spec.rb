d4cd4c132c6c6f98e5438615d1948fc7c003f788
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-05-04 16:50:57 +0300

    -

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,5 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature "Users", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "create user", type: :feature do
 end
