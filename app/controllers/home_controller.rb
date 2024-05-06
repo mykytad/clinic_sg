d550d17b6f020dcf6af11105a95ebec4ff05bfc7
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-05-06 12:27:03 +0300

    fix registration form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to categories_path
+    elsif doctor_signed_in?
+      redirect_to appointmets_path(doctor_id: current_doctor.id)
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
