ea45366a2de8cbd7afbb955111dc32d006677cf7
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-05-06 11:49:49 +0300

    fix doctor regestration

--- app/controllers/doctors/registrations_controller.rb
+++ app/controllers/doctors/registrations_controller.rb
@@ -46,7 +46,7 @@ class Doctors::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :category_id])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
